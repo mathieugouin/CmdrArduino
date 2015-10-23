a7ada19f426c5c622412835199c2d7764f4dd17c
mathieugouin <mgouin@gmail.com>
2015-10-23 00:08:24 -0400

    fix conversion

--- examples/CmdrArduino_minimum/CmdrArduino_minimum.ino
+++ examples/CmdrArduino_minimum/CmdrArduino_minimum.ino
@@ -35,26 +35,28 @@ void loop() {
   {
     //toggle!
     F0 ^= 1;
-    Serial.println(F0,BIN);
-    dps.setFunctions0to4(3,DCC_SHORT_ADDRESS,F0);
+    Serial.println(F0, BIN);
+    dps.setFunctions0to4(3, DCC_SHORT_ADDRESS, F0);
   }
   prev_state = button_state;
 
   //handle reading throttle
   analog_value = analogRead(0);
-  speed_byte = (analog_value >> 2)-127 ; //divide by four to take a 0-1023 range number and make it 1-126 range.
-  if(speed_byte != old_speed)
+  speed_byte = (analog_value >> 2) - 128 ; // Convert from [0, 1023] range to [-128, 127]
+  if (speed_byte != old_speed)
   {
-    if(speed_byte == 0) //this would be treated as an e-stop!
+    if (speed_byte == 0) //this would be treated as an e-stop!
     {
-      if(old_speed > 0) speed_byte = 1;
-      else speed_byte = -1;
+      if (old_speed > 0)
+        speed_byte = 1;
+      else
+        speed_byte = -1;
     }
     Serial.print("analog = ");
     Serial.println(analog_value, DEC);
     Serial.print("digital = ");
     Serial.println(speed_byte, DEC);
-    dps.setSpeed128(3,DCC_SHORT_ADDRESS,speed_byte);
+    dps.setSpeed128(3, DCC_SHORT_ADDRESS, speed_byte);
     old_speed = speed_byte;
   }
   dps.update();
